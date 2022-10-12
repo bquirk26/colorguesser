04c909d71146dde8018b70cd908aa8be6233c4f8
bquirk26 <bquirk26@amherst.edu>
2022-10-12 19:50:26 -0400

    some functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const generateHex = () => {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+  }
+
+  const [options, setOptions] = useState([generateHex(), generateHex(), generateHex(), generateHex()])
+  const [color, setColor] = useState(options[Math.floor(Math.random() * 4)]);
+  const [score, setScore] = useState(0);
+  console.log(color);
+
+  const update = () => {
+    let x = [generateHex(), generateHex(), generateHex(), generateHex()]
+    setOptions(x);
+    let n = x[Math.floor(Math.random() * 4)];
+    setColor(n);
+  }
+
+  const checkAnswer = (answer) => {
+      if (answer === color) {
+        setScore(score + 1);
+      }
+      update();
+  }
+
+
+
+
+  //@source https://css-tricks.com/snippets/javascript/random-hex-color/
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id = "main">
+      <h1>{score}</h1>
+      <div id = "viewer">
+        <div id = "view" style = {{"backgroundColor": `${color}`}}></div>
+        <div id = "options">
+          {options.map(val => <button key = {val} onClick = {() => checkAnswer(val)}>{val}</button>)}
+        </div>
+      </div>
+
     </div>
   );
 }
